3cd84267bfbe70d2512e7cf07824809ca59fde8e
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-27 00:58:19 +0300

    gcc correction

--- pop.c
+++ pop.c
@@ -15,11 +15,11 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%lu: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	temp->next->prev = NULL;
-	*stack = *stack->next;
+	*stack = temp->next;
 	free(temp);
 }
