82ec999faeb28d78eacc1c70a4dfc5b482a08143
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-27 05:09:02 +0300

    Add division by zero error to mod.c

--- mod.c
+++ mod.c
@@ -24,6 +24,12 @@ void modulate(stack_t **stack, unsigned int line_number)
 		clean();
 		exit(EXIT_FAILURE);
 	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		clean();
+		exit(EXIT_FAILURE);
+	}
 	(*stack)->next->n %= (*stack)->n;
 	pop(stack, line_number);
 }
